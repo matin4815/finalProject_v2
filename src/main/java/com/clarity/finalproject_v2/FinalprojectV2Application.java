f749173e1847244f1f8b784709cc37c2746e8e35
Matin NazariBaqa <matinnazaribaqa@gmail.com>
2022-01-05 17:21:27 +0330

    initial commit

---
+++ src/main/java/com/clarity/finalproject_v2/FinalprojectV2Application.java
@@ -0,0 +1,13 @@
+package com.clarity.finalproject_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalprojectV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalprojectV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/clarity/finalproject_v2/FinalprojectV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clarity.finalproject_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalprojectV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
